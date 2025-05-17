3f10b00a97fbdca945065afb82e9fa33cedac01a
edwhere <edw.delta@gmail.com>
2025-05-17 00:40:30 -0700

    Expanding functions to support inference mode.

--- datahandler.py
+++ datahandler.py
@@ -1,9 +1,39 @@
 
+import os
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
+from PIL import Image
+from constants import IMAGE_EXTENSIONS
 
 IMAGE_SIZE = 256  # Size of the input image (images are resized to this value)
 NUM_WORKERS = 4   # Number of workers for data loading
+
+
+class ImageFolderDataset(Dataset):
+    def __init__(self, dir_path, transform=None):
+        self.__dir = dir_path
+        self.__transform = transform
+
+        items = os.listdir(self.__dir)
+        images = [item for item in items if item.split(".")[-1] in IMAGE_EXTENSIONS]
+        self.__image_paths = [os.path.join(self.__dir, image) for image in images]
+
+        self.__num_images = len(images)
+
+    def __len__(self):
+        return self.__num_images
+
+    def __getitem__(self, idx):
+        img_path = self.__image_paths[idx]
+        img_fname = os.path.basename(img_path)
+        img_data = Image.open(img_path)
+        if self.__transform:
+            img_data = self.__transform(img_data)
+
+        return {
+            "image": img_data,
+            "filename": img_fname
+        }
 
 # Training transforms
 def get_trn_transform(image_size):
@@ -41,6 +71,10 @@ def get_datasets(trn_dir_path, val_dir_path):
     dataset_valid = datasets.ImageFolder(val_dir_path, transform=(get_val_transform(IMAGE_SIZE)))
     return dataset_train, dataset_valid, dataset_train.classes
 
+def get_infer_dataset(image_dir):
+    dataset_infer = ImageFolderDataset(dir_path=image_dir, transform=(get_val_transform(IMAGE_SIZE)))
+    return dataset_infer
+
 
 def get_data_loaders(dataset_train, dataset_valid, batch_size):
     """ Return the loader functions for TRN and VAL datasets. The function uses the dataset handlers
@@ -49,3 +83,10 @@ def get_data_loaders(dataset_train, dataset_valid, batch_size):
     train_loader = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=NUM_WORKERS)
     valid_loader = DataLoader(dataset_valid, batch_size=batch_size, shuffle=False, num_workers=NUM_WORKERS)
     return train_loader, valid_loader
+
+
+def get_infer_loader(dataset_infer, batch_size):
+    """ Return a loader function for inference operation."""
+    infer_loader = DataLoader(dataset_infer, batch_size=batch_size, shuffle=False, num_workers=NUM_WORKERS)
+    return infer_loader
+
--- model.py
+++ model.py
@@ -1,8 +1,11 @@
 """Implement a DINOv2 model with a different header."""
 
 import torch
-
 from collections import OrderedDict
+
+import constants as con
+import errordefs as err
+
 
 def build_model(num_classes: int=2, fine_tune: bool=False):
     backbone_model = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14')
@@ -20,14 +23,66 @@ def build_model(num_classes: int=2, fine_tune: bool=False):
 
     return model
 
+
+def load_stored_data(file_path: str):
+    """Load the model from file_path, initialize the model architecture and assign stored weights.
+    Return:
+        The instantiated model and the list of labels.
+    """
+
+    # Load stored model. This operation returns a dictionary.
+    model_data = torch.load(file_path, weights_only=False)
+
+    if 'storage_type' not in model_data.keys():
+        raise err.UnknownStorageFormat("Incompatible stored model format.")
+
+    if model_data['storage_type'] not in con.FORMAT_TYPES:
+        raise err.UnknownStorageFormat("Incompatible stored model format.")
+
+    if 'labels' not in model_data.keys():
+        raise err.UnknownStorageFormat("Incompatible stored model format.")
+
+    labels = model_data["labels"]
+    num_classes = len(labels)
+
+    # Instantiate model architecture
+    backbone_model = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14')
+
+    model = torch.nn.Sequential(OrderedDict([
+        ('backbone', backbone_model),
+        ('head', torch.nn.Linear(
+            in_features=384, out_features=num_classes, bias=True
+        ))
+    ]))
+
+    # Instantiate model weights
+    model.load_state_dict(state_dict=model_data['model_state_dict'])
+
+    return model, labels
+
+
+
 if __name__ == "__main__":
-    # Test script for model creation.
-    my_model = build_model()
-    print(my_model)
-    print('---------------------------------------------------------------')
+    test1 = False
+    test2 = True
 
-    total_params = sum(p.numel() for p in my_model.parameters())
-    print(f"total parameters: {total_params}")
+    if test1:
+        # Test script for model creation.
+        my_model = build_model()
+        print(my_model)
+        print('---------------------------------------------------------------')
 
-    trainable_params = sum(p.numel() for p in my_model.parameters() if p.requires_grad)
-    print(f"trainable parameters: {trainable_params}")
+        total_params = sum(p.numel() for p in my_model.parameters())
+        print(f"total parameters: {total_params}")
+
+        trainable_params = sum(p.numel() for p in my_model.parameters() if p.requires_grad)
+        print(f"trainable parameters: {trainable_params}")
+
+    elif test2:
+        file_loc = "/home/xuser/Experzone/Dino32/best_dv2model.pth"
+        loaded_model, loaded_labels = load_stored_data(file_loc)
+        print()
+        print("loaded labels:", loaded_labels)
+
+        total_params = sum(p.numel() for p in loaded_model.parameters())
+        print(f"total parameters: {total_params}")
